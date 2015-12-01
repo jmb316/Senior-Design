b1068ba74da2689d6e3f82861875c64be55d7a7b
Jennifer Barry <jennifer.morgan.barry@gmail.com>
2015-12-01 01:10:16 -0500

    another heroku fix

--- public/javascripts/chapterevents.js
+++ public/javascripts/chapterevents.js
@@ -9,7 +9,7 @@ twoHoursLater = twoHoursLater.toISOString();
 
 //TODO: get from config file
 //heroku:
-var clientid = '316615911187-duaavj04u4g1poomqp7dpm76pjuf2642.apps.googleusercontent.com';
+var clientId = '316615911187-duaavj04u4g1poomqp7dpm76pjuf2642.apps.googleusercontent.com';
 //local:
 //var clientId= '316615911187-g6qp9ghdv970gcnl4c2g6j90koov8chc.apps.googleusercontent.com';
 
